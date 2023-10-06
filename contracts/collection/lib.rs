04b72296bdbd4ec4e915fcbeec560ad78140abd8
Pierina Ponce <pierinaponce1993@gmail.com>
2023-10-06 00:03:54 -0400

    implements minting

--- contracts/collection/lib.rs
+++ contracts/collection/lib.rs
@@ -1,142 +1,75 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
-#[ink::contract]
-mod collection {
+#[openbrush::implementation(
+    Ownable,
+    PSP34,
+    PSP34Mintable,
+    PSP34Burnable,
+    PSP34Enumerable,
+    PSP34Metadata
+)]
+#[openbrush::contract]
+pub mod collection {
+    use openbrush::{
+        contracts::psp34::{extensions::metadata, PSP34Impl},
+        modifiers,
+        traits::Storage,
+    };
 
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
     #[ink(storage)]
+    #[derive(Default, Storage)]
     pub struct Collection {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+        #[storage_field]
+        psp34: psp34::Data,
+        next_id: u8,
+        #[storage_field]
+        ownable: ownable::Data,
+        #[storage_field]
+        metadata: metadata::Data,
+        #[storage_field]
+        enumerable: enumerable::Data,
     }
 
     impl Collection {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
+        pub fn new(name: String, symbol: String, base_uri: String) -> Self {
+            let mut instance = Self::default();
+            let caller = Self::env().caller();
+            ownable::Internal::_init_with_owner(&mut instance, caller);
+            let collection_id = PSP34Impl::collection_id(&instance);
+            metadata::Internal::_set_attribute(
+                &mut instance,
+                collection_id.clone(),
+                String::from("name"),
+                name,
+            );
+            metadata::Internal::_set_attribute(
+                &mut instance,
+                collection_id.clone(),
+                String::from("symbol"),
+                symbol,
+            );
+            metadata::Internal::_set_attribute(
+                &mut instance,
+                collection_id,
+                String::from("baseUri"),
+                base_uri,
+            );
+            instance
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
-        }
-
-        /// Simply returns the current value of our `bool`.
-        #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
-        }
-    }
-
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
-    #[cfg(test)]
-    mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let collection = Collection::default();
-            assert_eq!(collection.get(), false);
-        }
-
-        /// We test a simple use case of our contract.
-        #[ink::test]
-        fn it_works() {
-            let mut collection = Collection::new(false);
-            assert_eq!(collection.get(), false);
-            collection.flip();
-            assert_eq!(collection.get(), true);
-        }
-    }
-
-
-    /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
-    ///
-    /// When running these you need to make sure that you:
-    /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
-    /// - Are running a Substrate node which contains `pallet-contracts` in the background
-    #[cfg(all(test, feature = "e2e-tests"))]
-    mod e2e_tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// A helper function used for calling contract messages.
-        use ink_e2e::build_message;
-
-        /// The End-to-End test `Result` type.
-        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-        /// We test that we can upload and instantiate the contract using its default constructor.
-        #[ink_e2e::test]
-        async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = CollectionRef::default();
-
-            // When
-            let contract_account_id = client
-                .instantiate("collection", &ink_e2e::alice(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            // Then
-            let get = build_message::<CollectionRef>(contract_account_id.clone())
-                .call(|collection| collection.get());
-            let get_result = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
+        #[modifiers(only_owner)]
+        pub fn mint_token(&mut self) -> Result<(), PSP34Error> {
+            psp34::Internal::_mint_to(self, Self::env().caller(), Id::U8(self.next_id))?;
+            self.next_id += 1;
             Ok(())
         }
 
-        /// We test that we can read and write a value from the on-chain contract contract.
-        #[ink_e2e::test]
-        async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = CollectionRef::new(false);
-            let contract_account_id = client
-                .instantiate("collection", &ink_e2e::bob(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            let get = build_message::<CollectionRef>(contract_account_id.clone())
-                .call(|collection| collection.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            // When
-            let flip = build_message::<CollectionRef>(contract_account_id.clone())
-                .call(|collection| collection.flip());
-            let _flip_result = client
-                .call(&ink_e2e::bob(), flip, 0, None)
-                .await
-                .expect("flip failed");
-
-            // Then
-            let get = build_message::<CollectionRef>(contract_account_id.clone())
-                .call(|collection| collection.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), true));
-
-            Ok(())
+        #[ink(message)]
+        #[modifiers(only_owner)]
+        pub fn mint(&mut self, id: Id) -> Result<(), PSP34Error> {
+            psp34::Internal::_mint_to(self, Self::env().caller(), id)
         }
     }
 }
