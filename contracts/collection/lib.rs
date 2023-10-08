7dab4598c3bdd261ff403bea02efcd398c583f17
Pierina Ponce <pierinaponce1993@gmail.com>
2023-10-07 21:43:23 -0400

    new folder organization and testing

--- contracts/collection/lib.rs
+++ contracts/collection/lib.rs
@@ -1,10 +1,11 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
+#![feature(min_specialization)]
 
 #[openbrush::implementation(
     Ownable,
     PSP34,
-    PSP34Mintable,
     PSP34Burnable,
+    PSP34Mintable,
     PSP34Enumerable,
     PSP34Metadata
 )]
@@ -13,8 +14,10 @@ pub mod collection {
     use openbrush::{
         contracts::psp34::{extensions::metadata, PSP34Impl},
         modifiers,
-        traits::Storage,
+        traits::{Storage, String},
     };
+    use payable_mint_pkg::impls::payable_mint;
+    use payable_mint_pkg::impls::payable_mint::types;
 
     #[ink(storage)]
     #[derive(Default, Storage)]
@@ -28,11 +31,22 @@ pub mod collection {
         metadata: metadata::Data,
         #[storage_field]
         enumerable: enumerable::Data,
+        #[storage_field]
+        payable_mint: types::Data,
     }
+
+    impl payable_mint::payable_mint::PayableMintImpl for Collection {}
+    impl payable_mint::payable_mint::Internal for Collection {}
 
     impl Collection {
         #[ink(constructor)]
-        pub fn new(name: String, symbol: String, base_uri: String) -> Self {
+        pub fn new(
+            name: String,
+            symbol: String,
+            base_uri: String,
+            max_supply: u64,
+            price_per_mint: Balance,
+        ) -> Self {
             let mut instance = Self::default();
             let caller = Self::env().caller();
             ownable::Internal::_init_with_owner(&mut instance, caller);
@@ -55,6 +69,9 @@ pub mod collection {
                 String::from("baseUri"),
                 base_uri,
             );
+            instance.payable_mint.max_supply = max_supply;
+            instance.payable_mint.price_per_mint = price_per_mint;
+            instance.payable_mint.last_token_id = 0;
             instance
         }
 
@@ -65,11 +82,78 @@ pub mod collection {
             self.next_id += 1;
             Ok(())
         }
+    }
+}
 
-        #[ink(message)]
-        #[modifiers(only_owner)]
-        pub fn mint(&mut self, id: Id) -> Result<(), PSP34Error> {
-            psp34::Internal::_mint_to(self, Self::env().caller(), id)
-        }
+#[cfg(test)]
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::collection::Collection;
+    use ink::env::test;
+    use openbrush::contracts::traits::errors::PSP34Error::*;
+    use openbrush::{
+        contracts::psp34::{extensions::enumerable::*, extensions::metadata, PSP34Impl},
+        modifiers,
+        traits::{AccountId, Balance, Storage, String},
+    };
+    use payable_mint_pkg::impls::payable_mint::*;
+
+    const PRICE: Balance = 100_000_000_000_000_000;
+    fn init() -> Collection {
+        const BASE_URI: &str = "ipfs://myIpfsUri/";
+        const MAX_SUPPLY: u64 = 10;
+        Collection::new(
+            String::from("Collection"),
+            String::from("WMN"),
+            String::from(BASE_URI),
+            MAX_SUPPLY,
+            PRICE,
+        )
+    }
+
+    #[ink::test]
+    fn mint_multiple_works() {
+        let mut collection = init();
+        let accounts = test::default_accounts::<ink::env::DefaultEnvironment>();
+        set_sender(accounts.bob);
+        let num_of_mints: u64 = 5;
+
+        assert_eq!(PSP34Impl::total_supply(&collection), 0);
+        test::set_value_transferred::<ink::env::DefaultEnvironment>(PRICE * num_of_mints as u128);
+        assert!(
+            payable_mint::PayableMintImpl::mint(&mut collection, accounts.bob, num_of_mints)
+                .is_ok()
+        );
+        assert_eq!(PSP34Impl::total_supply(&collection), num_of_mints as u128);
+        assert_eq!(PSP34Impl::balance_of(&collection, accounts.bob), 5);
+        assert_eq!(
+            PSP34EnumerableImpl::owners_token_by_index(&collection, accounts.bob, 0),
+            Ok(Id::U64(1))
+        );
+        assert_eq!(
+            PSP34EnumerableImpl::owners_token_by_index(&collection, accounts.bob, 1),
+            Ok(Id::U64(2))
+        );
+        assert_eq!(
+            PSP34EnumerableImpl::owners_token_by_index(&collection, accounts.bob, 2),
+            Ok(Id::U64(3))
+        );
+        assert_eq!(
+            PSP34EnumerableImpl::owners_token_by_index(&collection, accounts.bob, 3),
+            Ok(Id::U64(4))
+        );
+        assert_eq!(
+            PSP34EnumerableImpl::owners_token_by_index(&collection, accounts.bob, 4),
+            Ok(Id::U64(5))
+        );
+        assert_eq!(
+            PSP34EnumerableImpl::owners_token_by_index(&collection, accounts.bob, 5),
+            Err(TokenNotExists)
+        );
+    }
+
+    fn set_sender(sender: AccountId) {
+        ink::env::test::set_caller::<ink::env::DefaultEnvironment>(sender);
     }
 }
---
+++ logics/impls/payable_mint/payable_mint.rs
@@ -0,0 +1,110 @@
+use crate::impls::payable_mint::types::Data;
+use crate::traits::payable_mint;
+pub use crate::traits::payable_mint::PayableMint;
+
+use ink::prelude::string::{String as PreludeString, ToString};
+use openbrush::contracts::psp34::extensions::metadata;
+use openbrush::traits::DefaultEnv;
+use openbrush::{
+    contracts::{ownable::*, psp34::*},
+    modifiers,
+    traits::{AccountId, Balance, Storage, String},
+};
+
+#[openbrush::trait_definition]
+pub trait PayableMintImpl:
+    Storage<Data>
+    + Storage<psp34::Data>
+    + Storage<ownable::Data>
+    + Storage<metadata::Data>
+    + Internal
+    + psp34::InternalImpl
+    + PSP34Impl
+    + metadata::Internal
+    + metadata::PSP34MetadataImpl
+{
+    #[ink(message, payable)]
+    fn mint(&mut self, to: AccountId, mint_amount: u64) -> Result<(), PSP34Error> {
+        self.check_value(Self::env().transferred_value(), mint_amount)?;
+        self.check_amount(mint_amount)?;
+
+        let next_to_mint = self.data::<Data>().last_token_id + 1; // first mint id is 1
+        let mint_offset = next_to_mint + mint_amount;
+
+        for mint_id in next_to_mint..mint_offset {
+            psp34::InternalImpl::_mint_to(self, to, Id::U64(mint_id))?;
+            self.data::<Data>().last_token_id += 1;
+        }
+
+        Ok(())
+    }
+
+    /// Withdraws funds to contract owner
+    #[ink(message)]
+    #[openbrush::modifiers(only_owner)]
+    fn withdraw(&mut self) -> Result<(), PSP34Error> {
+        let balance = Self::env().balance();
+        let current_balance = balance
+            .checked_sub(Self::env().minimum_balance())
+            .unwrap_or_default();
+        let owner = self.data::<ownable::Data>().owner.get().unwrap().unwrap();
+        Self::env()
+            .transfer(owner, current_balance)
+            .map_err(|_| PSP34Error::Custom(String::from("WithdrawalFailed")))?;
+        Ok(())
+    }
+
+    /// Set new value for the baseUri
+    #[ink(message)]
+    #[openbrush::modifiers(only_owner)]
+    fn set_base_uri(&mut self, uri: String) -> Result<(), PSP34Error> {
+        let id = PSP34Impl::collection_id(self);
+        metadata::Internal::_set_attribute(self, id, String::from("baseUri"), uri);
+
+        Ok(())
+    }
+
+    /// Get URI from token ID
+    #[ink(message)]
+    fn token_uri(&self, token_id: u64) -> Result<String, PSP34Error> {
+        self.token_exists(Id::U64(token_id))?;
+        let base_uri = metadata::PSP34MetadataImpl::get_attribute(
+            self,
+            PSP34Impl::collection_id(self),
+            String::from("baseUri"),
+        );
+        let token_uri = base_uri.unwrap() + &token_id.to_string() + &String::from(".json");
+        Ok(token_uri)
+    }
+}
+
+pub trait Internal: Storage<Data> + psp34::Internal {
+    /// Check if the transferred mint values is as expected
+    fn check_value(&self, transferred_value: u128, mint_amount: u64) -> Result<(), PSP34Error> {
+        if let Some(value) = (mint_amount as u128).checked_mul(self.data::<Data>().price_per_mint) {
+            if transferred_value == value {
+                return Ok(());
+            }
+        }
+        return Err(PSP34Error::Custom(String::from("BadMintValue")));
+    }
+
+    /// Check amount of tokens to be minted
+    fn check_amount(&self, mint_amount: u64) -> Result<(), PSP34Error> {
+        if mint_amount == 0 {
+            return Err(PSP34Error::Custom(String::from("CannotMintZeroTokens")));
+        }
+        if let Some(amount) = self.data::<Data>().last_token_id.checked_add(mint_amount) {
+            if amount <= self.data::<Data>().max_supply {
+                return Ok(());
+            }
+        }
+        return Err(PSP34Error::Custom(String::from("CollectionIsFull")));
+    }
+
+    // Check if token is minted
+    fn token_exists(&self, id: Id) -> Result<(), PSP34Error> {
+        self._owner_of(&id).ok_or(PSP34Error::TokenNotExists)?;
+        Ok(())
+    }
+}
