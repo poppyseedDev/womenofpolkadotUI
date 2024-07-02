7c45d9b526db1e8fcdc7b0de32b0f3e52910809d
Aurora Poppyseed <aurora.makovac@gmail.com>
2024-07-02 19:13:25 +0200

    landing page done

--- packages/frontend/src/app/SupportSection.tsx
+++ packages/frontend/src/app/SupportSection.tsx
@@ -5,10 +5,10 @@ const SupportSection: React.FC = () => {
   return (
     <>
       <div className="support-section bg-[url('/brand/supportcause.png')] bg-cover bg-center text-center text-white overlay">
-        <div className="bg-black bg-opacity-60 p-40"> {/* Overlay for better text readability */}
-          <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-6" style={{ fontFamily: "'Pixelify Sans', sans-serif" }}>Support Our Cause</h2>
-          <p className="text-base md:text-lg mb-6">Mint an NFT on Astar or Aleph Zero to support women in the Polkadot ecosystem.</p>
-          <button className="inline-block px-6 py-2 border border-white rounded-full text-white hover:bg-white hover:text-[#123456] transition duration-300 ease-in-out">Mint NFT to Support</button>
+        <div className="bg-black bg-opacity-60 p-40 flex flex-col justify-center items-center"> {/* Overlay for better text readability */}
+          <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-6" style={{ fontFamily: "'Pixelify Sans', sans-serif" }}>Stay Connected</h2>
+          <p className="text-base md:text-lg max-w-lg mb-6">Stay updated with the latest news, events, and insights. Check out our blog for more information on whats happening within our community.</p>
+          <a href="https://medium.com/women-of-polkadot" className="inline-block px-6 py-2 border border-white rounded-full text-white hover:bg-white hover:text-[#123456] transition duration-300 ease-in-out">Follow Our Blog</a>
         </div>
       </div>
     </>
